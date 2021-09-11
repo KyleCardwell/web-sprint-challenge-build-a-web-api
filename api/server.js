baa33cf4fc08c1735b57a2fc0b0e594b244e25bc
Kyle <kyle.e.cardwell@gmail.com>
2021-09-11 08:23:56 -0600

    basic server and route setup complete, cors, helmet, express, dotenv

--- api/server.js
+++ api/server.js
@@ -1,9 +1,24 @@
+require('dotenv').config()
+
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet')
+
+const actionsRoutes = require('./actions/actions-router')
+const projectsRoutes = require('./projects/projects-router')
+
 const server = express();
+
+server.use(express.json())
+server.use(cors())
+server.use(helmet())
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+// server.use('/api/actions', actionsRoutes)
+// server.use('/api/projects', projectsRoutes)
 
 module.exports = server;
